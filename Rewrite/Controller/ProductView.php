ad1498f6b3ce0968116de1e598290927b6779a04
sasha <alexander.pop.izmail@gmail.com>
2020-03-02 13:06:19 -0600

    Add Rewrite Logic

---
+++ Rewrite/Controller/ProductView.php
@@ -0,0 +1,11 @@
+<?php
+/* Glory to Ukraine! Glory to the heros! */
+
+namespace Codelegacy\FooCode\Rewrite\Controller;
+
+use Magento\Catalog\Controller\Product\View;
+
+class ProductView extends View
+{
+    
+}
