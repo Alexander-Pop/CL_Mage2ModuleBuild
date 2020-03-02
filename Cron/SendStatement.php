ae6c86a2969b11effc2a77118bae87c38c70d21e
sasha <alexander.pop.izmail@gmail.com>
2020-03-02 11:51:51 -0600

    Add Cron

---
+++ Cron/SendStatement.php
@@ -0,0 +1,20 @@
+<?php
+/* Glory to Ukraine! Glory to the heros! */
+
+namespace Codelegacy\FooCode\Cron;
+
+/**
+ * Cron class.
+ */
+class SendStatement
+{
+    public function __construct(
+        
+    ) {
+        
+    }
+
+    public function execute() {
+
+    }
+}
