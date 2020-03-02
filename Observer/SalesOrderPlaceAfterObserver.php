ac2d26b9a52f43fad8c84040523607a58f917b4b
sasha <alexander.pop.izmail@gmail.com>
2020-03-02 11:49:52 -0600

    Add Observer

---
+++ Observer/SalesOrderPlaceAfterObserver.php
@@ -0,0 +1,23 @@
+<?php
+/* Glory to Ukraine! Glory to the heros! */
+
+namespace Codelegacy\FooCode\Observer;
+
+use Magento\Framework\Event\ObserverInterface;
+
+/**
+ * Observer class.
+ */
+class SalesOrderPlaceAfterObserver implements ObserverInterface
+{
+    public function __construct(
+
+    ) {
+        
+    }
+
+    public function execute(\Magento\Framework\Event\Observer $observer)
+    {
+
+    }
+}
