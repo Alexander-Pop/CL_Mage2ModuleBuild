21e49f51ebe933d34008b1008d2a66cd28dafa77
sasha <alexander.pop.izmail@gmail.com>
2020-03-02 11:48:01 -0600

    Add plugin

---
+++ Plugin/ProductAttributesUpdater.php
@@ -0,0 +1,16 @@
+<?php
+/* Glory to Ukraine! Glory to the heros! */
+
+namespace Codelegacy\FooCode\Plugin;
+
+/**
+ * Plugin class.
+ */
+class ProductAttributesUpdater
+{
+    public function __construct(
+        
+    ) {
+        
+    }
+}
